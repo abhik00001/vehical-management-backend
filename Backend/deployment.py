8d366092c58fdb162d0248ec38603083b086b3d2
abhik00001 <sahilk786786123@gmail.com>
2025-08-15 17:22:45 +0530

    try superuser

--- Backend/deployment.py
+++ Backend/deployment.py
@@ -40,3 +40,10 @@ DATABASES = {
         conn_max_age=600,
     )
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
--- build.sh
+++ build.sh
@@ -4,5 +4,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
 python manage.py migrate
+
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+    echo "✅ Creating superuser from env vars..."
+    python manage.py createsuperuser --no-input
+else
+    echo "🔕 CREATE_SUPERUSER is not enabled. Skipping superuser creation."
+fi
