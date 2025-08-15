ae12dd0682a35829d366afff3b616579c8b7ca6e
abhik00001 <sahilk786786123@gmail.com>
2025-08-15 18:07:24 +0530

    try superuser

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ python manage.py migrate
 
 if [ "$CREATE_SUPERUSER" = "True" ]; then
     echo "✅ Creating superuser from env vars..."
-    python manage.py createsuperuser --no-input
+    python create_superuser.py
 else
     echo "🔕 CREATE_SUPERUSER is not enabled. Skipping superuser creation."
 fi
---
+++ create_superuser.py
@@ -0,0 +1,21 @@
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin1@gmail.com")
+first_name = os.environ.get("DJANGO_SUPERUSER_FIRSTNAME", "Admin")
+role = os.environ.get("DJANGO_SUPERUSER_ROLE", "admin")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin1")
+
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(
+        email=email,
+        first_name=first_name,
+        role=role,
+        password=password,
+    )
+    print("✅ Superuser created")
+else:
+    print("ℹ️ Superuser already exists")
