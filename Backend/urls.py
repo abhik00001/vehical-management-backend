f61f3196abfc21ed84935b664a141c6da4ca01ee
abhik00001 <sahilk786786123@gmail.com>
2025-08-17 14:22:01 +0530

    npm run dev

--- Backend/urls.py
+++ Backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from settings import BASE_DIR
+from Backend.settings import BASE_DIR
 
 urlpatterns = [
     path('admin/', admin.site.urls),
